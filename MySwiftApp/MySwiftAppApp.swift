e25e43f3531cb59b36f0ce5b8b77dd7d326dd01a
岩野隼也 <syun.0813@icloud.com>
2024-04-24 10:57:22 +0900

    Initial Commit

---
+++ MySwiftApp/MySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftAppApp.swift
+//  MySwiftApp
+//
+//  Created by iwano syunya on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
